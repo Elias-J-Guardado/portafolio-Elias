bf367c245dc56cf3d01f2bb089902144ab4f892d
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-01-26 14:09:31 -0600

    añadiendo preview de curriculum al modal y desactivando la barra superior del mismo, añadiendo funcionalidades a el boton descarga a traves de javascript

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,42 @@
 const modal = document.querySelector('#modal');
-const btnCV = document.querySelector('#btn-curriculum');
+const btnDescargar = document.querySelector('#descargar');
+
+
+const descargaLink = "./src/pdf/Luis_Elias_Guardado_Junior_Web_Developer_Resume.pdf";
+
+// addEventListeners
+btnDescargar.addEventListener('click', descargarCurriculum);
+
+function descargarCurriculum() {
+    alerta('Descargando...');
+
+    const link = document.createElement("a");
+    link.style.display = "none";
+    link.href = descargaLink;
+    link.setAttribute("download", "");
+    document.body.appendChild(link);
+    link.click();
+    document.removeChild(link);
+}
+
+function alerta(texto) {
+    const existeAlerta = document.querySelector('#alerta')
+
+    limpiarHTML(existeAlerta);
+
+    const alerta = document.createElement('P');
+    alerta.classList.add('text-center', 'text-danger');
+    alerta.textContent = texto;
+
+    existeAlerta.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
+
+function limpiarHTML(limpiar) {
+    while (limpiar.firstChild) {
+        limpiar.removeChild(limpiar.firstChild);
+    }
+}
