63baac36ce271b11e87ccaa3ed7214fc6e25f8a7
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-01-26 11:55:36 -0600

    añadiendo modal y corrigiendo errores

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,2 @@
 const modal = document.querySelector('#modal');
 const btnCV = document.querySelector('#btn-curriculum');
-
-btnCV.addEventListener('click', mostrarCurriculum);
-
-function mostrarCurriculum() {
-    
-     const crearDiv = document.createElement('DIV');
-     crearDiv.innerHTML = `
-         <div class="modal">
-            
-         </div
-     `
-}
