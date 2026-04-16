3482e320334a416268bb9a7f216c5ef60c30cff7
Elias <elias@localhost.localdomain>
2026-04-16 15:53:47 -0600

    añadiendo español a i18n.js

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -13,5 +13,22 @@ const translations = {
         "hero.cta": "Contactame",
         
         // Estudios
+        "education.itcha": "<strong>ITCHA</strong>-Tecnico en computación (2020)",
+        "education.alura": "<strong>ALURA LATAM</strong>-Data Scientist (2026)",
+
+        // Proyectos
+        "project.title": "Projectos",
+
+        // Globos&FloresAR
+        "globos&floresar.description": "Landing page creada con HTML.",
+        "globos&floresar.tecnology": "Tailwind y javascript.",
+        "globos&floresar.api": "Con manejo de API de Emailjs.",
+
+        // FlightOnTime
+        "flightontime.tecnology": "Programación de Frontend usando html, tailwind y vite.",
+        "flightontime.eda": "Analisis exploratirio de datos EDA.",
+        "flightontime.api": "Integración de frontend con API de predicción de vuelos retrasados.",
+
+        //
     }
 }
